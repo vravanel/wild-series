ad88e8e1d0b339c9c8a4a10e0ff1dc8d390d2010
vravanel <vravanel@gmail.com>
2023-05-23 12:27:21 +0200

    quete 3,4,5,6,7

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProgramController extends AbstractController
+{
+    #[Route('/program', name: 'program_index')]
+public function index(): Response
+{
+    return $this->render('program/index.html.twig', [
+       'website' => 'Wild Series',
+    ]);
+}
+
+    #[Route('/program/{id}', requirements: ['id'=>'\d+'], methods: ['GET'], name: 'program_show')]
+public function show(int $id): Response
+{
+    return $this->render('program/show.html.twig', ['controller_name' => 'ProgramController', 'program_id' => $id]);
+}
+}
