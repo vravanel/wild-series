15223d5f57a9817a1b3be1dc5e5c97515ecea132
vravanel <vravanel@gmail.com>
2023-06-10 16:52:47 +0200

    quete 23

--- src/Controller/EpisodeController.php
+++ src/Controller/EpisodeController.php
@@ -2,14 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Episode;
+use App\Form\CommentType;
 use App\Form\EpisodeType;
+use App\Repository\CommentRepository;
 use App\Repository\EpisodeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+
 
 #[Route('/episode')]
 class EpisodeController extends AbstractController
@@ -42,14 +47,30 @@ class EpisodeController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_episode_show', methods: ['GET'])]
-    public function show(Episode $episode): Response
+    #[Route('/{id}', name: 'app_episode_show', methods: ['GET', 'POST'])]
+    public function show(Request $request, CommentRepository $commentRepository, Episode $episode, Security $security): Response
     {
+        $comment = new Comment();
+        $comments = $commentRepository->findBy(['episode' => $episode], ['id' => 'ASC']);
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $security->getUser();
+            $comment->setUser($user);
+            $comment->setEpisode($episode);
+            $commentRepository->save($comment, true);
+            $this->addFlash('success', "Le commentaire a bien été ajouté");
+
+            return $this->redirectToRoute('app_episode_show', ['id' => $episode->getId()], Response::HTTP_SEE_OTHER);
+        }
+
         return $this->render('episode/show.html.twig', [
             'episode' => $episode,
+            'form' => $form,
+            'comments' => $comments,
         ]);
     }
-
     #[Route('/{id}/edit', name: 'app_episode_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Episode $episode, EpisodeRepository $episodeRepository): Response
     {
--- src/Entity/Episode.php
+++ src/Entity/Episode.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\EpisodeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -25,6 +27,14 @@ class Episode
 
     #[ORM\ManyToOne(inversedBy: 'episodes')]
     private ?Season $season = null;
+
+    #[ORM\OneToMany(mappedBy: 'episode', targetEntity: Comment::class)]
+    private Collection $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
 
     public function getId(): ?int
@@ -76,6 +86,36 @@ class Episode
     public function setSeason(?Season $season): self
     {
         $this->season = $season;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments->add($comment);
+            $comment->setEpisode($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getEpisode() === $this) {
+                $comment->setEpisode(null);
+            }
+        }
 
         return $this;
     }
